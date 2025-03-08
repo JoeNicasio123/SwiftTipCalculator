296a78f280c594712cbca79287fc2cf79f576e04
JoeNicasio123 <joenicasio123@gmail.com>
2025-03-08 12:29:53 -0600

    tip calculator layout

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billAmount: UITextField!
+    
+    @IBOutlet weak var tipAmount: UILabel!
+    
+    @IBOutlet weak var totalBill: UILabel!
+    
+    @IBAction func fifteenPercent(_ sender: Any) {
+        
+        }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
